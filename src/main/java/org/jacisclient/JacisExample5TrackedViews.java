e6169d6ab0b3d3c28661ee4c59aa73240b77af2d
Jan Wiemer <jan.wiemer@online,.de>
2020-12-29 17:06:22 +0100

    worked on documentation

--- src/main/java/org/jacisclient/JacisExample5TrackedViews.java
+++ src/main/java/org/jacisclient/JacisExample5TrackedViews.java
@@ -18,19 +18,18 @@ import org.jacisclient.JacisExample1GettingStarted.Account;
  *
  * @author Jan Wiemer
  */
-@SuppressWarnings({})
 public class JacisExample5TrackedViews {
 
   // Note that we use the same account object introduced for the first example
 
   public static void main(String[] args) {
     JacisContainer container = new JacisContainer();
-    JacisObjectTypeSpec<String, Account, Account> objectTypeSpec = new JacisObjectTypeSpec<>(String.class, Account.class, new JacisCloningObjectAdapter<>());
+    JacisObjectTypeSpec<String, Account, Account> objectTypeSpec //
+        = new JacisObjectTypeSpec<>(String.class, Account.class, new JacisCloningObjectAdapter<>());
     JacisStore<String, Account> store = container.createStore(objectTypeSpec).getStore();
 
     // First register the tracked view
-
-    store.getTrackedViewRegistry().registerTrackedView(new TotalBalanceView());
+    store.getTrackedViewRegistry().registerTrackedView("totalBalanceView", new TotalBalanceView());
 
     // First we create some accounts to have some test data...
 
@@ -41,7 +40,7 @@ public class JacisExample5TrackedViews {
     });
 
     // on internalCommit the tracked view is updated automatically
-    TotalBalanceView view0 = store.getTrackedViewRegistry().getView(TotalBalanceView.class);
+    TotalBalanceView view0 = store.getTrackedViewRegistry().getView("totalBalanceView");
     System.out.println("tracked balance=" + view0.getTotalBalance());
 
     // inside a transaction the transaction local view of the values is respected
@@ -49,12 +48,12 @@ public class JacisExample5TrackedViews {
       store.update("account1", store.get("account1").deposit(1000));
       store.update("account4", new Account("account4").deposit(101));
       // note that the getView method takes a snapshot at the time it is called...
-      TotalBalanceView view1 = store.getTrackedViewRegistry().getView(TotalBalanceView.class);
+      TotalBalanceView view1 = store.getTrackedViewRegistry().getView("totalBalanceView");
       System.out.println("tracked balance=" + view1.getTotalBalance());
       // later updates are not tracked by this snapshot
       store.update("account1", store.get("account1").deposit(1000));
       System.out.println("tracked balance old snapshot=" + view1.getTotalBalance());
-      TotalBalanceView view2 = store.getTrackedViewRegistry().getView(TotalBalanceView.class);
+      TotalBalanceView view2 = store.getTrackedViewRegistry().getView("totalBalanceView");
       System.out.println("tracked balance new snapshot=" + view2.getTotalBalance());
     });
 
